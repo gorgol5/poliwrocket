89e148775504e676a6415245de58930bc1005b68
Michał Gorgolewski <gorgolewskimichal@gmail.com>
2020-09-14 20:39:30 +0200

    v 0.4.2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,54 +1,71 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {
-    Link
-} from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
 
-const Menu = (props) => (<nav id="menu" >
-    <div className="inner" >
-        <ul className="links" >
-            <li >
-                < Link onClick={
-                    props.onToggleMenu
-                }
-                    to="/" > Home
-                    </Link>
-            </li >
-            <li >
-                < Link onClick={
-                    props.onToggleMenu
-                }
-                    to="/rockets" > Nasze rakiety
-                    </Link>
-            </li >
-            <li > < Link onClick={
-                props.onToggleMenu
-            }
-                to="/about" > O nas </Link></li >
-            <li > < Link onClick={
-                props.onToggleMenu
-            }
-                to="/team" > Nasz Team </Link></li >
-            <li > < Link onClick={
-                props.onToggleMenu
-            }
-                to="/sponsors" > Sponsorzy </Link></li >
-            <li > < Link onClick={
-                props.onToggleMenu
-            }
-                to="/news" > Newsy </Link></li >
-        </ul> <ul className="actions vertical" >
-            <li > < a href="#"
-                className="button special fit" > Zaczynajmy </a></li >
-        </ul> </div> <a className="close"
-            onClick={
-                props.onToggleMenu
-            }
-            href="javascript:;" > Close </a> </nav>
-)
+const Menu = (props) => (
+	<nav id="menu">
+		<div className="inner">
+			<ul className="links">
+				<li>
+					<Link onClick={props.onToggleMenu} to="/">
+						{' '}
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link onClick={props.onToggleMenu} to="/rockets">
+						{' '}
+						Nasze rakiety
+					</Link>
+				</li>
+				<li>
+					{' '}
+					<Link onClick={props.onToggleMenu} to="/about">
+						{' '}
+						O nas{' '}
+					</Link>
+				</li>
+				<li>
+					{' '}
+					<Link onClick={props.onToggleMenu} to="/team">
+						{' '}
+						Nasz Team{' '}
+					</Link>
+				</li>
+				<li>
+					{' '}
+					<Link onClick={props.onToggleMenu} to="/sponsors">
+						{' '}
+						Sponsorzy{' '}
+					</Link>
+				</li>
+				<li>
+					{' '}
+					<Link onClick={props.onToggleMenu} to="/news">
+						{' '}
+						Newsy{' '}
+					</Link>
+				</li>
+			</ul>{' '}
+			<ul className="actions vertical">
+				<li>
+					{' '}
+					<a href="#one" onClick={props.onToggleMenu} className=" button special fit">
+						{' '}
+						Zaczynajmy{' '}
+					</a>
+				</li>
+			</ul>{' '}
+		</div>{' '}
+		<a className="close" onClick={props.onToggleMenu} href="javascript:;">
+			{' '}
+			Close{' '}
+		</a>{' '}
+	</nav>
+);
 
 Menu.propTypes = {
-    onToggleMenu: PropTypes.func
-}
+	onToggleMenu: PropTypes.func
+};
 
-export default Menu
+export default Menu;
